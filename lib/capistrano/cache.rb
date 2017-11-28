3f43af668c1530ea9716777368c7f475debffe59
Piotr Kędziak <piotr@kedziak.com>
2017-11-28 14:23:35 +0100

    load tasks for Capistrano 3

--- lib/capistrano/cache.rb
+++ lib/capistrano/cache.rb
@@ -1,4 +1,3 @@
-module Capistrano
-  module Cache
-  end
+if Gem::Specification.find_by_name('capistrano').version >= Gem::Version.new('3.0.0')
+  load File.expand_path('../tasks/cache.rake', __FILE__)
 end
