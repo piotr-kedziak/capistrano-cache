14423d22c94973c9fd577e791c141165d134887e
Piotr Kędziak <piotr@kedziak.com>
2017-11-28 15:06:14 +0100

    refactoring

--- lib/capistrano/cache.rb
+++ lib/capistrano/cache.rb
@@ -1,3 +1,5 @@
+require 'tasks/app/cache.rake'
+
 if Gem::Specification.find_by_name('capistrano').version >= Gem::Version.new('3.0.0')
-  load File.expand_path('../tasks/cache.rake', __FILE__)
+  load File.expand_path('tasks/cache.rake', __FILE__)
 end
